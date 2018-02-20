6f1260b8fdea0ca5798dfe4b1adeb630b8178c53
Helen G. Kunda <hgkunda@users.noreply.github.com>
2018-02-20 15:39:58 -0500

    added filters

--- report.php
+++ report.php
@@ -10,15 +10,15 @@
 
 <?php #Script 1.0 - report.php
 
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
+    $first_name = filter_input(INPUT_POST, 'first_name');
+    $last_name = filter_input(INPUT_POST, 'last_name');
+    $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
     $when_happen = $_POST['when_happen'];
     $how_many = $_POST['how_many'];
     $how_long = $_POST['how_long'];
     $describe = $_POST['describe'];
     $what_do = $_POST['what_do'];
-    $seen_fluffy = $_POST['seen_fluffy'];
+    $seen_fluffy = filter_input(INPUT_POST, 'seen_fluffy'];
     $comments = $_POST['comments'];
 
   echo "<p>Thanks for submitting the form <strong>$first_name <?nbsp> $last_name</strong>.</p><p>You were abducted on <strong>$when_happen</strong> and gone for <strong>$how_long</strong>.</p><p>You said there were <strong>$how_many</strong> of them.</p><p>And they <strong>$what_do</strong>.</p><p>You described them as <strong>$describe</strong>.</p><p>Did you see Fluffy? You answered: <strong>$seen_fluffy</strong></p><p>Your other comments were: <strong>$comments</strong></p><p>We will contact you at <strong>$email</strong> if we have any relevant news.</p>";
