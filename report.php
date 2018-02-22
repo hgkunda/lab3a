0368321a933aa7d75cf244f23d218600a26d4ef2
Helen G. Kunda <hgkunda@users.noreply.github.com>
2018-02-22 12:36:04 -0500

    ughhh

--- report.php
+++ report.php
@@ -23,20 +23,22 @@
 
 
 
-    if (!$first_name)
+    if (!$first_name) {
         $error_fname = "ERROR! You must enter a vaild first name";
-
-    if (!$last_name)
+}
+    if (!$last_name) {
         $error_lname = "ERROR! You must enter a valid last name";
-
-    if (!$email)
+}
+    if (!$email) {
         $error_email = "ERROR! You must enter a valid email address";
-
-    if (!$seen_fluffy)
+}
+    if (!$seen_fluffy) {
         $error_fluffy = "ERROR! Please let us know if you have seen our dear little Fluffy (we really miss him)!";
+        }
 
-
+    if ($first_name && $last_name && $email && $seen_fluffy) {
          echo "<p>Thanks for submitting the form <strong> $first_name $error_fname</strong>  <strong>$last_name $error_lname </strong>.</p><p>You were abducted on <strong> $when_happen </strong> and gone for <strong> $how_long </strong>.</p><p>You said there were <strong> $how_many </strong> of them.</p><p>And they <strong>$what_do</strong>.</p><p>You described them as <strong> $describe </strong>.</p><p>Did you see Fluffy? You answered: <strong> $seen_fluffy $error_fluffy </strong></p><p>Your other comments were: <strong> $comments </strong></p><p>We will contact you at <strong> $email $error_email </strong> if we have any relevant news.</p>";
+        }
 ?>
 
 </body>
